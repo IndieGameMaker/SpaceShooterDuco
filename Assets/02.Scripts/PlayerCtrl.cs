801900838c10c9786c9d58f4884f3540e15d15b9
Lee Jae Hyun <myleje@gmail.com>
2021-10-09 14:31:50 +0900

    Project upload

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    // 화면을 랜더링하는 주기
+    void Update()
+    {
+
+    }
+}
