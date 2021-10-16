32f9cc888c2c9fa611be13107dd34f14706ef738
Lee Jae Hyun <myleje@gmail.com>
2021-10-16 17:31:58 +0900

    .

--- Assets/02.Scripts/Monster.cs
+++ Assets/02.Scripts/Monster.cs
@@ -15,6 +15,12 @@ public class Monster : MonoBehaviour
     private Transform monsterTr;
     private Transform playerTr;
 
+    // 추적사정거리 , 공격사정거리 변수
+    public float traceDist = 10.0f;
+    public float attackDist = 2.0f;
+
+    public bool isDie = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,11 +31,34 @@ public class Monster : MonoBehaviour
         {
             playerTr = playerObj.GetComponent<Transform>(); //playerObj.transform;
         }
+
+        StartCoroutine(CheckMonsterState());
     }
 
-    // Update is called once per frame
-    void Update()
+    // 몬스터의 상태를 체크하는 코루틴
+    IEnumerator CheckMonsterState()
     {
+        while (!isDie)
+        {
+            // 몬스터의 상태를 체크하는 로직
+            // 주인공의 위치와 몬스터의 위치값으로 거리 계산
+            float distance = Vector3.Distance(playerTr.position, monsterTr.position);
 
+            if (distance <= attackDist)
+            {
+                // 상태를 공격으로 변경
+                state = State.ATTACK;
+            }
+            else if (distance <= traceDist)
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.IDLE;
+            }
+
+            yield return new WaitForSeconds(0.3f);
+        }
     }
 }
