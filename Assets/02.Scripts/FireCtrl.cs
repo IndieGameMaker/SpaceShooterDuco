c8e2cf9bb6c1be77b8a1ecb1be66a9d88a2e2a52
Lee Jae Hyun <myleje@gmail.com>
2021-10-16 13:46:32 +0900

    총소리 로직

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -1,21 +1,38 @@
+#pragma warning disable CS0108, IDE0052, IDE0051
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
     public GameObject bulletPrefab; // Bullet 프리팹을 저장할 변수
     public Transform firePos;       // 총알을 생성할 위치 정보
+    public AudioClip fireSfx;       // 총소리 음원
+
+    private AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            // 총알을 생성 
-            // 객체 생성 Instantiate(생성할객체, 위치, 각도)
-            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+            Fire();
         }
+    }
 
+    void Fire()
+    {
+        // 총알을 생성 
+        // 객체 생성 Instantiate(생성할객체, 위치, 각도)
+        Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        // 총소리 발생
+        audio.PlayOneShot(fireSfx, 0.8f);
     }
 }
