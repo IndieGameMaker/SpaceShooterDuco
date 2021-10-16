55157a79d59542e05c757faf41181174a2c7e647
Lee Jae Hyun <myleje@gmail.com>
2021-10-16 09:41:30 +0900

    .

---
+++ Assets/02.Scripts/FireCtrl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePos;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
