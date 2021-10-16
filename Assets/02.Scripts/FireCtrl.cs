a4b0657408b4f833bb73d4f4ae64f55bf8e8fbc6
Lee Jae Hyun <myleje@gmail.com>
2021-10-16 09:47:52 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class FireCtrl : MonoBehaviour
 {
-    public GameObject bulletPrefab;
-    public Transform firePos;
-
-    void Start()
-    {
-
-    }
+    public GameObject bulletPrefab; // Bullet 프리팹을 저장할 변수
+    public Transform firePos;       // 총알을 생성할 위치 정보
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 총알을 생성 
+            // 객체 생성 Instantiate(생성할객체, 위치, 각도)
+            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        }
 
     }
 }
