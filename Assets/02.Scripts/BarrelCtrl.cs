c80d00bf4061fabf562b66d3bce8d56672436da2
Lee Jae Hyun <myleje@gmail.com>
2021-10-16 11:38:40 +0900

    .

---
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelCtrl : MonoBehaviour
+{
+    private int hitCount;
+
+    void OnCollisionEnter(Collision coll)
+    {
+        //if (coll.collider.tag == "문자열") // X
+        if (coll.collider.CompareTag("BULLET"))
+        {
+            if (++hitCount == 3)
+            {
+                // 드럼통 폭발하는 로직
+                ExpBarrel();
+            }
+        }
+    }
+
+    void ExpBarrel()
+    {
+        // Rigidbody 컴포넌트 추가
+        Rigidbody rb = this.gameObject.AddComponent<Rigidbody>();
+        rb.AddForce(Vector3.up * 1500.0f);
+        Destroy(this.gameObject, 3.0f);
+    }
+}
+
+/*
+    C# - Managed Language : 메모리 활당 / 해제 자동으로 처리한 언어 - Garbage Collection
+    C,C++ - Unmanaged Language 
+*/
