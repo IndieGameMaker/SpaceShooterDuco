fa24e342169b4415e9eafd8a8b3a9710f15a2e88
Lee Jae Hyun <myleje@gmail.com>
2021-10-17 13:49:39 +0900

    CreateMonster 구현

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -7,15 +7,23 @@ public class GameManager : MonoBehaviour
     public GameObject monsterPrefab;
     public Transform[] points;
 
+    public bool isGameOver;
+    public float createTime = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         points = GameObject.Find("SpawnPointGroup").GetComponentsInChildren<Transform>();
+
+        InvokeRepeating("CreateMonster", 2.0f, createTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateMonster()
     {
+        // 출현시킬 위치정보 Index 추출
+        int idx = Random.Range(1, points.Length); //1~23
 
+        // 몬스터 생성
+        Instantiate(monsterPrefab, points[idx].position, Quaternion.identity);
     }
 }
